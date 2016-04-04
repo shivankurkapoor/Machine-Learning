86b8285c36c5b2024f8a7e19bb44146535e1ce6b
shivankurkapoor <shivankurkapoor3192@gmail.com>
2016-04-03 20:21:42 -0700

    Exercise - ML by Andrew Ng

---
+++ machine-learning-ex7/machine-learning-ex7/ex7/computeCentroids.m
@@ -0,0 +1,37 @@
+function centroids = computeCentroids(X, idx, K)
+%COMPUTECENTROIDS returs the new centroids by computing the means of the 
+%data points assigned to each centroid.
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   computing the means of the data points assigned to each centroid. It is
+%   given a dataset X where each row is a single data point, a vector
+%   idx of centroid assignments (i.e. each entry in range [1..K]) for each
+%   example, and K, the number of centroids. You should return a matrix
+%   centroids, where each row of centroids is the mean of the data points
+%   assigned to it.
+%
+
+% Useful variables
+[m n] = size(X);
+
+% You need to return the following variables correctly.
+centroids = zeros(K, n);
+
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every centroid and compute mean of all points that
+%               belong to it. Concretely, the row vector centroids(i, :)
+%               should contain the mean of the data points assigned to
+%               centroid i.
+%
+% Note: You can use a for-loop over the centroids to compute this.
+%
+for i = 1:K
+%  tmp1 = repmat([i:i],m,1);
+%  tmp2 = repmat((idx == tmp1),1,n);
+%  count = sum((idx == tmp1));
+%  tmp3 = tmp2.*X;
+ centroids(i:i,:) = sum((repmat((idx == (repmat(i:i,m,1))),1,n)).*X)/(sum((idx == (repmat(i:i,m,1)))));
+% =============================================================
+
+end
+
